f880300c36404dc5cc708d78322afdd79dc1e24c
dworthen <dworthen@wsu.edu>
2018-06-07 08:57:45 -0700

    Initial commit

---
+++ src/BlazorRealm/ExampleJsInterop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace BlazorRealm
+{
+    public class ExampleJsInterop
+    {
+        public static string Prompt(string message)
+        {
+            return RegisteredFunction.Invoke<string>(
+                "BlazorRealm.ExampleJsInterop.Prompt",
+                message);
+        }
+    }
+}
