bcbf6b960a0efd46d24e8ac8fb2e241a8aae791f
Worthen <dworthen@wsu.edu>
2019-06-19 13:34:25 -0700

    fix: Update Client side example

---
+++ examples/BlazorClientApp/Redux/Actions.cs
@@ -0,0 +1,87 @@
+﻿using Blazor.Realm;
+using Blazor.Realm.Async;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorClientApp.Redux.Actions
+{
+    namespace Counter
+    {
+        public class IncrementByValue : IRealmAction
+        {
+            public int Value { get; set; }
+            public IncrementByValue(int value)
+            {
+                Value = value;
+            }
+        }
+
+        public class DecrementByValue : IRealmAction
+        {
+            public int Value { get; set; }
+            public DecrementByValue(int value)
+            {
+                Value = value;
+            }
+        }
+
+        public class Reset : IRealmAction { }
+
+        public class Dispose : Reset { }
+    }
+
+    namespace WeatherForecasts
+    {
+        public class Clear : IRealmAction { }
+
+        public class Set : IRealmAction
+        {
+            public IEnumerable<WeatherForecast> Value { get; set; }
+            public Set(IEnumerable<WeatherForecast> forecasts)
+            {
+                Value = forecasts;
+            }
+        }
+
+        public class FetchAsync : IAsyncRealmAction
+        {
+            public Store<AppState> Store { get; set; }
+            public HttpClient Client { get; set; }
+
+            public FetchAsync(Store<AppState> store, HttpClient httpClient)
+            {
+                Store = store ?? throw new ArgumentNullException(nameof(store));
+                Client = httpClient ?? throw new ArgumentNullException(nameof(store));
+            }
+
+            public async Task Invoke()
+            {
+                Store.Dispatch(new IsLoading.Set(true));
+                Store.Dispatch(new WeatherForecasts.Clear());
+
+                await Task.Delay(1000);
+                WeatherForecast[] forecasts = await Client.GetJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+
+                Store.Dispatch(new WeatherForecasts.Set(forecasts));
+                Store.Dispatch(new IsLoading.Set(false));
+            }
+        }
+    }
+
+    namespace IsLoading
+    {
+        public class Set : IRealmAction
+        {
+            public bool Value { get; set; }
+
+            public Set(bool value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
--- examples/BlazorClientApp/Startup.cs
+++ examples/BlazorClientApp/Startup.cs
@@ -1,28 +1,30 @@
-﻿using Blazor.Realm;
+using Blazor.Realm;
 using Blazor.Realm.Async.Extensions;
 using Blazor.Realm.Extensions;
 using Blazor.Realm.ReduxDevTools.Extensions;
-using Microsoft.AspNetCore.Blazor.Builder;
+using BlazorClientApp.Redux;
+using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 
-namespace BlazorStandAlone
+namespace BlazorClientApp
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSingleton<IJSRuntime>();
+            services.AddRealmReduxDevToolServices();
             services.AddRealmStore<AppState>(new AppState(), Reducers.RootReducer);
         }
 
-        public void Configure(IBlazorApplicationBuilder app, IStoreBuilder<AppState> RealmStoreBuilder)
+        public void Configure(IComponentsApplicationBuilder app, IStoreBuilder<AppState> RealmStoreBuilder)
         {
-            //app.Services.GetService<>
-
             RealmStoreBuilder.UseRealmAsync<AppState>();
 
             RealmStoreBuilder.UseRealmReduxDevTools<AppState>(new System.Type[]
             {
-                typeof(Actions.Counter.Dispose)
+                typeof(Redux.Actions.Counter.Dispose)
             });
 
             app.AddComponent<App>("app");
